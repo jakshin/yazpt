25a5bc4b036b0bd08e545deeb08a8d662e8f1271
Jason Jackson <jasonjackson@pobox.com>
2020-02-03 17:35:20 -0700

    Tests for merging in a git repo

---
+++ tests/git-merge.zsh
@@ -0,0 +1,26 @@
+#!/bin/zsh
+# Tests for merging in a git repo.
+
+# Initialize
+script_name="$(basename -- "$0")"
+cd -- "$(dirname -- "$0")"
+source ./utils.zsh
+before_tests $script_name true
+
+# Test
+test_case "Merging"
+git checkout merge-me
+git checkout master
+git merge merge-me
+test_init_done
+contains_branch "master"
+contains_status "dirty"
+contains "MERGING"
+cd .git
+test_init_done "no-status"
+contains_dim_branch "master"
+contains_status "unknown"
+contains "MERGING"
+
+# Clean up
+after_tests
