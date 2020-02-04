95fedd2adc6ba5cfec89cd130e93a00ab8de6930
Jason Jackson <jasonjackson@pobox.com>
2020-02-03 17:35:20 -0700

    Tests for bisecting in a git repo

---
+++ tests/git-bisect.zsh
@@ -0,0 +1,25 @@
+#!/bin/zsh
+# Tests for bisecting in a git repo.
+
+# Initialize
+script_name="$(basename -- "$0")"
+cd -- "$(dirname -- "$0")"
+source ./utils.zsh
+before_tests $script_name true
+
+# Test
+test_case "Bisecting"
+git checkout branch1
+git bisect start
+test_init_done
+contains_branch "branch1"
+contains_status "perfect"
+contains "BISECTING"
+cd .git
+test_init_done "no-status"
+contains_dim_branch "branch1"
+contains_status "unknown"
+contains "BISECTING"
+
+# Clean up
+after_tests
