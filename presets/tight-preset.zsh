fe9ffb3e6090151edc264b268f27a9562856e916
Jason Jackson <jasonjackson@pobox.com>
2020-02-28 21:13:31 -0700

    Add a one-line preset

---
+++ presets/tight-preset.zsh
@@ -0,0 +1,6 @@
+# A preset almost like the default, but on a single line,
+# and without a blank line before the prompt.
+
+source "$yazpt_default_preset_file"
+
+YAZPT_LAYOUT='[<cwd><? ><result><? ><git>]%# '
