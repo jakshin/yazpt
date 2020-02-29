162511e552e8f828407b503089adf2bdfe0e0b00
Jason Jackson <jasonjackson@pobox.com>
2020-02-28 21:13:14 -0700

    Fix the header comments

--- presets/spearmint-preset.zsh
+++ presets/spearmint-preset.zsh
@@ -1,11 +1,5 @@
-# A prompt preset based on my personal default settings for the last decade or so.
-#
-# It's maybe a bit weird to put the git branch and status on the left,
-# so it shifts the current directory to the right when it's visible,
-# but I like that it makes it more visually obvious when I'm in a git repo.
-#
-# Showing the current directory in bright yellow makes a very clear visual divider
-# between each command, since commands rarely use bright yellow in their output.
+# A preset using shades of green and bluish green,
+# with refreshing bright white input.
 
 source "$yazpt_default_preset_file"
 
