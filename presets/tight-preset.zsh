9ae31e8f3d1fa8ac818549b982a0227e0fa35de9
Jason Jackson <jasonjackson@pobox.com>
2020-03-02 20:37:40 -0700

    A space-conserving single-line preset
    It's like the default preset, but all on one line, and with no blank line before the prompt.

--- presets/tight-preset.zsh
+++ presets/tight-preset.zsh
@@ -1,4 +1,4 @@
-# A preset almost like the default, but on a single line,
+# A space-conserving preset almost like the default, but on a single line,
 # and without a blank line before the prompt.
 
 source "$yazpt_default_preset_file"
