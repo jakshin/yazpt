0a634589ebf91f882795e0621734c285616065b4
Jason Jackson <jasonjackson@pobox.com>
2020-03-03 23:34:55 -0700

    Tests for cherry-picking in a git repo

--- tests/git-cherry-pick.zsh
+++ tests/git-cherry-pick.zsh
@@ -7,6 +7,14 @@ cd -- "$(dirname -- "$0")"
 source ./utils.zsh
 before_tests $script_name true
 
+# Recent versions of git end "cherry-picking mode" earlier, probably starting in v2.25.1:
+# https://github.com/git/git/commit/f233c9f4550a831a69892e0a38db2a7654beb995
+version=$(git --version | awk '{ print $3 }')
+v=("${(@s/./)version}")
+if (( v[1] > 2 || (v[1] == 2 && v[2] >= 25) )); then
+	recent_git=true
+fi
+
 # Test
 test_case "Cherry-picking"
 git checkout cherry-pick-from-me
@@ -20,12 +28,12 @@ git add . && git commit -m "resolved"
 test_init_done
 contains_branch "master"
 contains_status "diverged"
-contains "CHERRY-PICKING"
+[[ $recent_git == true ]] || contains "CHERRY-PICKING"
 cd .git
 test_init_done
 contains_dim_branch "master"
-contains_status "unknown"
-contains "CHERRY-PICKING"
+contains_status "diverged"
+[[ $recent_git == true ]] || contains "CHERRY-PICKING"
 
 # Clean up
 after_tests
