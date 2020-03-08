ce8a01c84f944f6338db0efdc67b682df73ec81d
Jason Jackson <jasonjackson@pobox.com>
2020-03-08 16:04:32 -0600

    Tests for rebasing in a git repo

--- tests/git-rebase.zsh
+++ tests/git-rebase.zsh
@@ -8,52 +8,62 @@ source ./utils.zsh
 before_tests $script_name true
 
 # Test
-test_case "Rebasing interactively"
-export GIT_EDITOR=true
-git checkout rebase-me
-git rebase -i master
-dir_exists ".git/rebase-merge"          # Sanity check
-dir_does_not_exist ".git/rebase-apply"  # Sanity check
-test_init_done
-contains_branch "rebase-me"
-contains_status "dirty"
-contains "REBASING 1/2"
-cd .git
-test_init_done
-contains_dim_branch "rebase-me"
-contains_status "dirty"
-contains "REBASING 1/2"
-unset GIT_EDITOR
-cd .. && git rebase --abort  # Cleanup
+function run_tests() {
+	local git_dir=$(git rev-parse --git-dir)
 
-test_case "Rebasing"
-git checkout rebase-me
-git rebase master
-dir_does_not_exist ".git/rebase-merge"  # Sanity check
-dir_exists ".git/rebase-apply"          # Sanity check
-test_init_done
-contains_branch "rebase-me"
-contains_status "dirty"
-contains "REBASING 1/2"
-cd .git
-test_init_done
-contains_dim_branch "rebase-me"
-contains_status "dirty"
-contains "REBASING 1/2"
-cd .. && git rebase --abort  # Cleanup
+	test_case "Rebasing interactively"
+	export GIT_EDITOR=true
+	git checkout rebase-me
+	git rebase -i master
+	dir_exists "$git_dir/rebase-merge"          # Sanity check
+	dir_does_not_exist "$git_dir/rebase-apply"  # Sanity check
+	test_init_done
+	contains_branch "rebase-me"
+	contains_status "dirty"
+	contains "REBASING 1/2"
+	cd $git_dir
+	test_init_done
+	contains_dim_branch "rebase-me"
+	contains_status "dirty"
+	contains "REBASING 1/2"
+	unset GIT_EDITOR
+	cd - && git rebase --abort  # Cleanup
 
-test_case "Rebased branch that hasn't been pushed"
-git checkout rebase-me-too
-git rebase master
-test_init_done
-contains_branch "rebase-me-too"
-contains_status "diverged"
-cd .git
-test_init_done
-contains_dim_branch "rebase-me-too"
-contains_status "diverged"
-contains "IN-GIT-DIR"
-cd .. && git checkout master  # Cleanup
+	test_case "Rebasing"
+	git checkout rebase-me
+	git rebase master
+	dir_does_not_exist "$git_dir/rebase-merge"  # Sanity check
+	dir_exists "$git_dir/rebase-apply"          # Sanity check
+	test_init_done
+	contains_branch "rebase-me"
+	contains_status "dirty"
+	contains "REBASING 1/2"
+	cd $git_dir
+	test_init_done
+	contains_dim_branch "rebase-me"
+	contains_status "dirty"
+	contains "REBASING 1/2"
+	cd - && git rebase --abort  # Cleanup
+
+	test_case "Rebased branch that hasn't been pushed"
+	git checkout rebase-me-too
+	git rebase master
+	test_init_done
+	contains_branch "rebase-me-too"
+	contains_status "diverged"
+	cd $git_dir
+	test_init_done
+	contains_dim_branch "rebase-me-too"
+	contains_status "diverged"
+	contains "IN-GIT-DIR"
+	cd - && git checkout master  # Cleanup
+}
+
+run_tests
+
+# Reinitialize and test again, in a linked worktree
+before_linked_tests $script_name
+run_tests
 
 # Clean up
 after_tests
