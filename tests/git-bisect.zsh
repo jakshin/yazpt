720759ae63e355523559d6a69c94fe01431d5425
Jason Jackson <jasonjackson@pobox.com>
2020-03-08 16:03:01 -0600

    Tests for bisecting in a git repo

--- tests/git-bisect.zsh
+++ tests/git-bisect.zsh
@@ -8,18 +8,26 @@ source ./utils.zsh
 before_tests $script_name true
 
 # Test
-test_case "Bisecting"
-git checkout branch1
-git bisect start
-test_init_done
-contains_branch "branch1"
-contains_status "clean"
-contains "BISECTING"
-cd .git
-test_init_done
-contains_dim_branch "branch1"
-contains_status "clean"
-contains "BISECTING"
+function run_tests() {
+	test_case "Bisecting"
+	git checkout branch1
+	git bisect start
+	test_init_done
+	contains_branch "branch1"
+	contains_status "clean"
+	contains "BISECTING"
+	cd $(git rev-parse --git-dir)
+	test_init_done
+	contains_dim_branch "branch1"
+	contains_status "clean"
+	contains "BISECTING"
+}
+
+run_tests
+
+# Reinitialize and test again, in a linked worktree
+before_linked_tests $script_name
+run_tests
 
 # Clean up
 after_tests
