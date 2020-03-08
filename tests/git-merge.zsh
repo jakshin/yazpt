6de0084acf34336a72ac787eb4804ce6253c1493
Jason Jackson <jasonjackson@pobox.com>
2020-03-08 16:06:09 -0600

    Tests for merging in a git repo

--- tests/git-merge.zsh
+++ tests/git-merge.zsh
@@ -8,19 +8,27 @@ source ./utils.zsh
 before_tests $script_name true
 
 # Test
-test_case "Merging"
-git checkout merge-me
-git checkout master
-git merge merge-me
-test_init_done
-contains_branch "master"
-contains_status "dirty"
-contains "MERGING"
-cd .git
-test_init_done
-contains_dim_branch "master"
-contains_status "dirty"
-contains "MERGING"
+function run_tests() {
+	test_case "Merging"
+	git checkout merge-me
+	git checkout master
+	git merge merge-me
+	test_init_done
+	contains_branch "master"
+	contains_status "dirty"
+	contains "MERGING"
+	cd $(git rev-parse --git-dir)
+	test_init_done
+	contains_dim_branch "master"
+	contains_status "dirty"
+	contains "MERGING"
+}
+
+run_tests
+
+# Reinitialize and test again, in a linked worktree
+before_linked_tests $script_name
+run_tests
 
 # Clean up
 after_tests
