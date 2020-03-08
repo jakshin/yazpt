944236225326f50c5609426ddd872c789eb3c820
Jason Jackson <jasonjackson@pobox.com>
2020-03-08 16:07:17 -0600

    Tests for new git repos with no commits

--- tests/new-git-repo.zsh
+++ tests/new-git-repo.zsh
@@ -15,7 +15,7 @@ contains_branch "master"
 contains_status "no-upstream"
 
 test_case "In the .git directory of a new repo with no branches/commits"
-cd .git
+cd $(git rev-parse --git-dir)
 test_init_done
 contains_dim_branch "master"
 contains_status "no-upstream"
