2b6bf5e12d4a6b0fb427df289eadc853d6fc66ad
Jason Jackson <jasonjackson@pobox.com>
2020-03-08 16:02:49 -0600

    Tests for applying patches from email in a git repo

--- tests/git-am.zsh
+++ tests/git-am.zsh
@@ -8,23 +8,31 @@ source ./utils.zsh
 before_tests $script_name true
 
 # Test
-test_case "Applying patches from email (AM)"
-git checkout master
-git am patches/*
-test_init_done
-contains_branch "master"
-contains_status "clean"
-contains "AM 1/2"
-echo bleh >> foo.txt && git add . && git am --continue
-test_init_done
-contains_branch "master"
-contains_status "diverged"
-contains "AM 2/2"
-cd .git
-test_init_done
-contains_dim_branch "master"
-contains_status "diverged"
-contains "AM 2/2"
+function run_tests() {
+	test_case "Applying patches from email (AM)"
+	git checkout master
+	git am patches/*
+	test_init_done
+	contains_branch "master"
+	contains_status "clean"
+	contains "AM 1/2"
+	echo bleh >> foo.txt && git add . && git am --continue
+	test_init_done
+	contains_branch "master"
+	contains_status "diverged"
+	contains "AM 2/2"
+	cd $(git rev-parse --git-dir)
+	test_init_done
+	contains_dim_branch "master"
+	contains_status "diverged"
+	contains "AM 2/2"
+}
+
+run_tests
+
+# Reinitialize and test again, in a linked worktree
+before_linked_tests $script_name
+run_tests
 
 # Clean up
 after_tests
