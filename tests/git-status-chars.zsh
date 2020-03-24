dff2f977c63e6660a0a7f6adf5139f401055cdbb
Jason Jackson <jasonjackson@pobox.com>
2020-03-24 14:47:35 -0600

    Tests for our handling of Git status characters

--- tests/git-status-chars.zsh
+++ tests/git-status-chars.zsh
@@ -1,13 +1,14 @@
 #!/bin/zsh
-# Tests for our handling of git status characters.
+# Tests for our handling of Git status characters.
 
 # Initialize
 script_name="$(basename -- "$0")"
 cd -- "$(dirname -- "$0")"
 source ./utils.zsh
-before_tests $script_name true
+before_tests $script_name "git"
+YAZPT_VCS_ORDER=(git)
 
-function gitx() {
+function git_mock() {
 	if [[ $1 == "status" ]]; then
 		return 1
 	else
@@ -16,76 +17,76 @@ function gitx() {
 }
 
 function reset_git_status_chars() {
-	YAZPT_GIT_STATUS_CLEAN_CHAR="●"
-	YAZPT_GIT_STATUS_DIRTY_CHAR="⚑"
-	YAZPT_GIT_STATUS_DIVERGED_CHAR="◆"
-	YAZPT_GIT_STATUS_LINKED_BARE_CHAR="⚭"
-	YAZPT_GIT_STATUS_NO_UPSTREAM_CHAR="◆"
-	YAZPT_GIT_STATUS_UNKNOWN_CHAR="⌀"
+	YAZPT_VCS_STATUS_CLEAN_CHAR="●"
+	YAZPT_VCS_STATUS_DIRTY_CHAR="⚑"
+	YAZPT_VCS_STATUS_DIVERGED_CHAR="◆"
+	YAZPT_VCS_STATUS_LINKED_BARE_CHAR="⚭"
+	YAZPT_VCS_STATUS_NO_UPSTREAM_CHAR="◆"
+	YAZPT_VCS_STATUS_UNKNOWN_CHAR="⌀"
 }
 
 # Test
 function run_tests() {
-	test_case "Empty YAZPT_GIT_STATUS_CLEAN_CHAR"
+	test_case "Empty YAZPT_VCS_STATUS_CLEAN_CHAR"
 	reset_git_status_chars
 	test_init_done
 	contains "●"
-	contains "%F{$YAZPT_GIT_STATUS_CLEAN_CHAR_COLOR}"
-	YAZPT_GIT_STATUS_CLEAN_CHAR=""
+	contains "%F{$YAZPT_VCS_STATUS_CLEAN_COLOR}"
+	YAZPT_VCS_STATUS_CLEAN_CHAR=""
 	test_init_done
-	excludes_status
-	excludes "%F{$YAZPT_GIT_STATUS_CLEAN_CHAR_COLOR}"
+	excludes_git_status
+	excludes "%F{$YAZPT_VCS_STATUS_CLEAN_COLOR}"
 
-	test_case "Empty YAZPT_GIT_STATUS_DIRTY_CHAR"
+	test_case "Empty YAZPT_VCS_STATUS_DIRTY_CHAR"
 	reset_git_status_chars
 	touch new-file.txt
 	test_init_done
 	contains "⚑"
-	contains "%F{$YAZPT_GIT_STATUS_DIRTY_CHAR_COLOR}"
-	YAZPT_GIT_STATUS_DIRTY_CHAR=""
+	contains "%F{$YAZPT_VCS_STATUS_DIRTY_COLOR}"
+	YAZPT_VCS_STATUS_DIRTY_CHAR=""
 	test_init_done
-	excludes_status
-	excludes "%F{$YAZPT_GIT_STATUS_DIRTY_CHAR_COLOR}"
-	excludes "%F{$YAZPT_GIT_STATUS_CLEAN_CHAR_COLOR}"
+	excludes_git_status
+	excludes "%F{$YAZPT_VCS_STATUS_DIRTY_COLOR}"
+	excludes "%F{$YAZPT_VCS_STATUS_CLEAN_COLOR}"
 
-	test_case "Empty YAZPT_GIT_STATUS_DIVERGED_CHAR"
+	test_case "Empty YAZPT_VCS_STATUS_DIVERGED_CHAR"
 	reset_git_status_chars
 	git add . && git commit -m "Add a new file"
 	test_init_done
 	contains "◆"
-	contains "%F{$YAZPT_GIT_STATUS_DIVERGED_CHAR_COLOR}"
-	YAZPT_GIT_STATUS_DIVERGED_CHAR=""
+	contains "%F{$YAZPT_VCS_STATUS_DIVERGED_COLOR}"
+	YAZPT_VCS_STATUS_DIVERGED_CHAR=""
 	test_init_done
-	excludes_status
-	excludes "%F{$YAZPT_GIT_STATUS_DIVERGED_CHAR_COLOR}"
-	excludes "%F{$YAZPT_GIT_STATUS_CLEAN_CHAR_COLOR}"
+	excludes_git_status
+	excludes "%F{$YAZPT_VCS_STATUS_DIVERGED_COLOR}"
+	excludes "%F{$YAZPT_VCS_STATUS_CLEAN_COLOR}"
 
-	test_case "Empty YAZPT_GIT_STATUS_NO_UPSTREAM_CHAR"
+	test_case "Empty YAZPT_VCS_STATUS_NO_UPSTREAM_CHAR"
 	reset_git_status_chars
 	git checkout -b new-branch
 	test_init_done
 	contains "◆"
-	contains "%F{$YAZPT_GIT_STATUS_NO_UPSTREAM_CHAR_COLOR}"
-	YAZPT_GIT_STATUS_NO_UPSTREAM_CHAR=""
+	contains "%F{$YAZPT_VCS_STATUS_NO_UPSTREAM_COLOR}"
+	YAZPT_VCS_STATUS_NO_UPSTREAM_CHAR=""
 	test_init_done
-	excludes_status
-	excludes "%F{$YAZPT_GIT_STATUS_NO_UPSTREAM_CHAR_COLOR}"
-	excludes "%F{$YAZPT_GIT_STATUS_CLEAN_CHAR_COLOR}"
+	excludes_git_status
+	excludes "%F{$YAZPT_VCS_STATUS_NO_UPSTREAM_COLOR}"
+	excludes "%F{$YAZPT_VCS_STATUS_CLEAN_COLOR}"
 
-	test_case "Empty YAZPT_GIT_STATUS_UNKNOWN_CHAR"
+	test_case "Empty YAZPT_VCS_STATUS_UNKNOWN_CHAR"
 	reset_git_status_chars
-	alias git=gitx
+	alias git=git_mock
 	test_init_done
 	contains "⌀"
-	contains "%F{$YAZPT_GIT_STATUS_UNKNOWN_CHAR_COLOR}"
-	YAZPT_GIT_STATUS_UNKNOWN_CHAR=""
+	contains "%F{$YAZPT_VCS_STATUS_UNKNOWN_COLOR}"
+	YAZPT_VCS_STATUS_UNKNOWN_CHAR=""
 	test_init_done
-	excludes_status
-	excludes "%F{$YAZPT_GIT_STATUS_UNKNOWN_CHAR_COLOR}"
-	excludes "%F{$YAZPT_GIT_STATUS_CLEAN_CHAR_COLOR}"
+	excludes_git_status
+	excludes "%F{$YAZPT_VCS_STATUS_UNKNOWN_COLOR}"
+	excludes "%F{$YAZPT_VCS_STATUS_CLEAN_COLOR}"
 	unalias git
 
-	test_case "Empty YAZPT_GIT_STATUS_LINKED_BARE_CHAR"
+	test_case "Empty YAZPT_VCS_STATUS_LINKED_BARE_CHAR"
 	reset_git_status_chars
 	rm -rf * .git .gitignore
 	git clone --bare "https://github.com/jakshin/yazpt-test.git"
@@ -94,13 +95,13 @@ function run_tests() {
 	cd ../yazpt-linked
 	test_init_done
 	contains "⚭"
-	contains "%F{$YAZPT_GIT_STATUS_LINKED_BARE_CHAR_COLOR}"
-	YAZPT_GIT_STATUS_LINKED_BARE_CHAR=""
+	contains "%F{$YAZPT_VCS_STATUS_LINKED_BARE_COLOR}"
+	YAZPT_VCS_STATUS_LINKED_BARE_CHAR=""
 	test_init_done
-	excludes_status
-	excludes "%F{$YAZPT_GIT_STATUS_LINKED_BARE_CHAR_COLOR}"
-	excludes "%F{$YAZPT_GIT_STATUS_NO_UPSTREAM_CHAR_COLOR}"
-	excludes "%F{$YAZPT_GIT_STATUS_CLEAN_CHAR_COLOR}"
+	excludes_git_status
+	excludes "%F{$YAZPT_VCS_STATUS_LINKED_BARE_COLOR}"
+	excludes "%F{$YAZPT_VCS_STATUS_NO_UPSTREAM_COLOR}"
+	excludes "%F{$YAZPT_VCS_STATUS_CLEAN_COLOR}"
 }
 
 run_tests
