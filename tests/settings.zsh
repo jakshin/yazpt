e3cfee9ef91b58cf752b528218c2bbd978945fc1
Jason Jackson <jasonjackson@pobox.com>
2020-03-24 14:49:40 -0600

    Tests for settings (via environment variables)

--- tests/settings.zsh
+++ tests/settings.zsh
@@ -5,15 +5,7 @@
 script_name="$(basename -- "$0")"
 cd -- "$(dirname -- "$0")"
 source ./utils.zsh
-before_tests $script_name true
-
-function gitx() {
-	if [[ $1 == "status" ]]; then
-		return 1
-	else
-		command git "$@"
-	fi
-}
+before_tests $script_name
 
 # Test
 test_case "Current working directory color"
@@ -21,92 +13,39 @@ YAZPT_CWD_COLOR=5
 test_init_done
 contains '%{%F{5}%}%~%{%f%}'
 
-test_case "Git branch color"
-YAZPT_GIT_BRANCH_COLOR=1
-YAZPT_GIT_BRANCH_GIT_DIR_COLOR=2
-YAZPT_GIT_BRANCH_IGNORED_DIR_COLOR=3
-test_init_done
-contains '%{%F{1}%}master%{%f%}'
-cd $(git rev-parse --git-dir)
-test_init_done
-contains '%{%F{2}%}master|IN-GIT-DIR%{%f%}'
-mkdir ../ignored
-cd ../ignored
-test_init_done
-contains '%{%F{3}%}master|IGNORED%{%f%}'
-
-test_case "Git status = clean"
-YAZPT_GIT_STATUS_CLEAN_CHAR="★"
-YAZPT_GIT_STATUS_CLEAN_CHAR_COLOR=17
-test_init_done
-contains '%{%F{17}%}★%{%f%}'
-
-test_case "Git status = dirty"
-YAZPT_GIT_STATUS_DIRTY_CHAR="☆"
-YAZPT_GIT_STATUS_DIRTY_CHAR_COLOR=18
-touch new-file
-test_init_done
-contains '%{%F{18}%}☆%{%f%}'
-
-test_case "Git status = diverged"
-YAZPT_GIT_STATUS_DIVERGED_CHAR="✪"
-YAZPT_GIT_STATUS_DIVERGED_CHAR_COLOR=19
-git add . && git commit -m "new file"
-test_init_done
-contains '%{%F{19}%}✪%{%f%}'
-
-test_case "Git status = no remote/upstream branch"
-YAZPT_GIT_STATUS_NO_UPSTREAM_CHAR="✡︎"
-YAZPT_GIT_STATUS_NO_UPSTREAM_CHAR_COLOR=20
-git checkout -b new-branch
-test_init_done
-contains '%{%F{20}%}✡︎%{%f%}'
-
-test_case "Git status = unknown ('git status' failed)"
-YAZPT_GIT_STATUS_UNKNOWN_CHAR="✣"
-YAZPT_GIT_STATUS_UNKNOWN_CHAR_COLOR=21
-alias git=gitx
-test_init_done
-contains '%{%F{21}%}✣%{%f%}'
-unalias git
-
-test_case "Command result = error"
-YAZPT_RESULT_ERROR_CHAR="✦"
-YAZPT_RESULT_ERROR_CHAR_COLOR=9
-YAZPT_RESULT_ERROR_CODE_COLOR=13
-YAZPT_RESULT_ERROR_CODE_VISIBLE=true
+test_case "Command exit code = error"
+YAZPT_EXIT_ERROR_CHAR="✦"
+YAZPT_EXIT_ERROR_COLOR=9
+YAZPT_EXIT_ERROR_CODE_VISIBLE=true
 test_init_done "(exit 42)"
 contains '%{%F{9}%}✦%{%f%}'
-contains '%{%F{13}%}42%{%f%}'
-YAZPT_RESULT_ERROR_CHAR=""
+contains '%{%F{9}%}42%{%f%}'
+YAZPT_EXIT_ERROR_CHAR=""
 test_init_done "(exit 42)"
-excludes '%F{9}'
 excludes '✘'
-contains '%{%F{13}%}42%{%f%}'
-YAZPT_RESULT_ERROR_CHAR="✦"
-YAZPT_RESULT_ERROR_CODE_VISIBLE=false
+contains '%{%F{9}%}42%{%f%}'
+YAZPT_EXIT_ERROR_CHAR="✦"
+YAZPT_EXIT_ERROR_CODE_VISIBLE=false
 test_init_done "(exit 42)"
 contains '%{%F{9}%}✦%{%f%}'
-excludes '%F{13}'
 excludes '42'
 
-test_case "Command result = success"
-YAZPT_RESULT_OK_CHAR="✧"
-YAZPT_RESULT_OK_CHAR_COLOR=10
-YAZPT_RESULT_OK_CODE_COLOR=14
-YAZPT_RESULT_OK_CODE_VISIBLE=true
+test_case "Command exit code = success"
+YAZPT_EXIT_OK_CHAR="✧"
+YAZPT_EXIT_OK_COLOR=10
+YAZPT_EXIT_OK_CODE_VISIBLE=true
 test_init_done "true"
 contains '%{%F{10}%}✧%{%f%}'
-contains '%{%F{14}%}0%{%f%}'
-YAZPT_RESULT_OK_CHAR=""
+contains '%{%F{10}%}0%{%f%}'
+YAZPT_EXIT_OK_CHAR=""
 test_init_done "true"
-excludes '%F{10}'
-contains '%{%F{14}%}0%{%f%}'
-YAZPT_RESULT_OK_CHAR="✧"
-YAZPT_RESULT_OK_CODE_VISIBLE=false
+excludes '✧'
+contains '%{%F{10}%}0%{%f%}'
+YAZPT_EXIT_OK_CHAR="✧"
+YAZPT_EXIT_OK_CODE_VISIBLE=false
 test_init_done "true"
 contains '%{%F{10}%}✧%{%f%}'
-excludes '%F{14}'
+excludes '}0'
 
 # Clean up
 after_tests
