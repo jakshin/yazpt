7ba6284414b128ac4e31b3f4ff8e662bea9acae4
Jason Jackson <jasonjackson@pobox.com>
2020-03-24 14:59:28 -0600

    Tests for yazpt's path prefix whitelists

---
+++ tests/whitelist.zsh
@@ -0,0 +1,48 @@
+#!/bin/zsh
+# Tests for yazpt's path prefix whitelists.
+
+# Initialize
+script_name="$(basename -- "$0")"
+cd -- "$(dirname -- "$0")"
+source ./utils.zsh
+before_tests $script_name
+
+function test_whitelist() {
+	array_name=$1
+	if yazpt_test_whitelist $array_name; then
+		echo "allowed"
+	else
+		echo "rejected"
+	fi
+}
+
+# Test
+test_case "Empty whitelist allows any path"
+empty_array=()
+result=$(test_whitelist empty_array)
+equals "Whitelist result" $result "allowed"
+
+test_case "Whitelist with short prefix of current path allows it"
+whitelist=(dummy $TMPDIR)
+result=$(test_whitelist whitelist)
+equals "Whitelist result" $result "allowed"
+
+test_case "Whitelist with different prefix rejects the current directory"
+whitelist=(/foo ~/Documents)
+result=$(test_whitelist whitelist)
+equals "Whitelist result" $result "rejected"
+
+test_case "Whitelist matching current directory allows it"
+cwd="$(pwd)"
+whitelist=($cwd)
+result=$(test_whitelist whitelist)
+equals "Whitelist result" $result "allowed"
+
+test_case "Whitelist matching current directory, but with trailing slash, rejects it"
+cwd="$(pwd)/"
+whitelist=($cwd)
+result=$(test_whitelist whitelist)
+equals "Whitelist result" $result "rejected"
+
+# Clean up
+after_tests
