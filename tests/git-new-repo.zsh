2d6047ece754a11fc69b5318010d4214ac05aa50
Jason Jackson <jasonjackson@pobox.com>
2020-03-24 14:42:39 -0600

    Tests for new Git repos with no commits

--- tests/git-new-repo.zsh
+++ tests/git-new-repo.zsh
@@ -1,11 +1,12 @@
 #!/bin/zsh
-# Tests for new git repos with no commits.
+# Tests for new Git repos with no commits.
 
 # Initialize
 script_name="$(basename -- "$0")"
 cd -- "$(dirname -- "$0")"
 source ./utils.zsh
 before_tests $script_name
+YAZPT_VCS_ORDER=(git)
 
 # Test
 test_case "New repo with no branches/commits"
