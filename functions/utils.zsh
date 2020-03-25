9d4281aa976f988da833cc1da125a2e5ee2a5a34
Jason Jackson <jasonjackson@pobox.com>
2020-03-25 10:34:05 -0600

    Utility functions for use by yazpt's other functions

--- functions/utils.zsh
+++ functions/utils.zsh
@@ -1,9 +1,9 @@
-# Utility functions. These are only means to be called by other yazpt_* functions.
+# Utility functions. These are only means to be called by yazpt's other functions.
 
 # Details one Git/Subversion status; shows its character, in its configured color, and a description.
 # If the character is empty, it's shown as a gray "n/a".
 #
-function _yazpt_detail_vcs_status() {
+function .yazpt_detail_vcs_status() {
 	local color=$1 ch=$2 var=$3 desc=$4
 	[[ -z $5 ]] || desc+=" $5"
 
@@ -19,39 +19,39 @@ function _yazpt_detail_vcs_status() {
 		desc+=" Unset $var to keep this status from showing."
 	fi
 
-	_yazpt_print_wrapped $desc true
+	.yazpt_print_wrapped $desc true
 }
 
 # Makes a command string which can be invoked to wrap text,
-# and stores it as $yazpt_wrap_cmd.
+# and stores it as $_yazpt_wrap_cmd.
 #
-function _yazpt_make_wrap_cmd() {
+function .yazpt_make_wrap_cmd() {
 	local width=$(( $COLUMNS - 8 ))
 	(( $width >= 50 )) || width=50
 	(( $width <= 100 )) || width=100
 
 	if which "fmt" &> /dev/null; then
-		yazpt_wrap_cmd="fmt -w $width"
+		_yazpt_wrap_cmd="fmt -w $width"
 	elif which "fold" &> /dev/null; then
-		yazpt_wrap_cmd="fold -sw $width"
+		_yazpt_wrap_cmd="fold -sw $width"
 	else
-		yazpt_wrap_cmd=""
+		_yazpt_wrap_cmd=""
 	fi
 }
 
 # Prints some text, after wrapping it (so it might be printed on multiple lines).
 # You can choose whether or not to indent the 2nd+ lines with a tab.
 #
-function _yazpt_print_wrapped() {
+function .yazpt_print_wrapped() {
 	local text=$1
 	local tab_indent=$2  # Boolean
 
-	if (( $+yazpt_wrap_cmd == 0 )); then
-		_yazpt_make_wrap_cmd
+	if (( $+_yazpt_wrap_cmd == 0 )); then
+		.yazpt_make_wrap_cmd
 	fi
 
-	if [[ -n $yazpt_wrap_cmd ]]; then
-		local wrapped=(${(f)"$(echo "$text" | ${=yazpt_wrap_cmd})"})
+	if [[ -n $_yazpt_wrap_cmd ]]; then
+		local wrapped=(${(f)"$(echo "$text" | ${=_yazpt_wrap_cmd})"})
 	else
 		local wrapped=("$text")
 	fi
@@ -66,16 +66,16 @@ function _yazpt_print_wrapped() {
 
 # Prints some header text, after wrapping it.
 #
-function _yazpt_print_wrapped_header() {
+function .yazpt_print_wrapped_header() {
 	echo -n "\e[1m"
-	_yazpt_print_wrapped "$1"
+	.yazpt_print_wrapped "$1"
 	echo -n "\e[0m"
 }
 
 # Prints some warning/alert text, after wrapping it.
 #
-function _yazpt_print_wrapped_warning() {
+function .yazpt_print_wrapped_warning() {
 	echo -n "\e[38;5;217m"
-	_yazpt_print_wrapped "$1"
+	.yazpt_print_wrapped "$1"
 	echo -n "\e[0m"
 }
