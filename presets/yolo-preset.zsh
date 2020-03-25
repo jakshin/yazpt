3f070135abc0ed6bff038d74f3a968c8f3ae19d3
Jason Jackson <jasonjackson@pobox.com>
2020-03-25 10:36:42 -0600

    A preset with emoji and randomized but complementary colors

--- presets/yolo-preset.zsh
+++ presets/yolo-preset.zsh
@@ -1,11 +1,11 @@
-# A preset with emoji and randomized but complimentary colors.
+# A preset with emoji and randomized but complementary colors.
 # The cwd and git branch colors are different each time the preset is loaded,
 # then stay constant within that terminal session.
 
 emulate -L zsh
-declare -a yazpt_yolo_color_ranges=('22-39' '58-75' '94-111' '130-147')
+declare -a _yazpt_yolo_color_ranges=('22-39' '58-75' '94-111' '130-147')
 
-function yazpt_random_int() {
+function .yazpt_random_int() {
 	local lowest=$1
 	local highest=$2
 	local rand=$(hexdump -e '"%u"' -n 1 /dev/urandom)  # Seems better than $RANDOM
@@ -13,34 +13,34 @@ function yazpt_random_int() {
 	echo $((rand % modulo + lowest))
 }
 
-function yazpt_random_char() {
+function .yazpt_random_char() {
 	local str=$1
-	local rand=$(yazpt_random_int 1 $#str)
+	local rand=$(.yazpt_random_int 1 $#str)
 	echo $str[$rand]
 }
 
-function yazpt_random_color() {
-	local rand=$(yazpt_random_int 1 $#yazpt_yolo_color_ranges)
-	local range=$yazpt_yolo_color_ranges[$rand]
+function .yazpt_random_color() {
+	local rand=$(.yazpt_random_int 1 $#_yazpt_yolo_color_ranges)
+	local range=$_yazpt_yolo_color_ranges[$rand]
 	echo $range | IFS=- read -A range
-	yazpt_random_int $range[1] $range[2]	
+	.yazpt_random_int $range[1] $range[2]	
 }
 
 source "$yazpt_default_preset_file"
 
 YAZPT_LAYOUT=$'\n<exit><? ><cwd><? ><vcs>\n%# '
-YAZPT_CWD_COLOR=$(yazpt_random_color)
+YAZPT_CWD_COLOR=$(.yazpt_random_color)
 
-YAZPT_EXIT_ERROR_CHAR=$(yazpt_random_char "😫😖😬")
+YAZPT_EXIT_ERROR_CHAR=$(.yazpt_random_char "😫😖😬")
 YAZPT_EXIT_ERROR_COLOR=""
 YAZPT_EXIT_ERROR_CODE_VISIBLE=false
 
-YAZPT_EXIT_OK_CHAR=$(yazpt_random_char "🤘🤙👌")
+YAZPT_EXIT_OK_CHAR=$(.yazpt_random_char "🤘🤙👌")
 YAZPT_EXIT_OK_COLOR=""
 YAZPT_EXIT_OK_CODE_VISIBLE=false
 
 YAZPT_VCS_BRANCH_COLOR=$((YAZPT_CWD_COLOR + 6))
 YAZPT_VCS_WRAPPER_CHARS="❨❩"
 
-unfunction yazpt_random_int yazpt_random_char yazpt_random_color
-unset yazpt_yolo_color_ranges
+unfunction .yazpt_random_int .yazpt_random_char .yazpt_random_color
+unset _yazpt_yolo_color_ranges
