8e7572ae4850ae4a997ce36b9394973f549555c5
Jason Jackson <jasonjackson@pobox.com>
2020-03-28 16:46:23 -0600

    Run all test suites with short output, list any suites with failures

--- tests/all.zsh
+++ tests/all.zsh
@@ -6,7 +6,9 @@ cd -- "$(dirname -- "$0")"
 source ./utils.zsh
 summaries="$(mktemp)"
 
+declare -a failing_suites=()
 first=true
+
 for suite in *.zsh; do
 	if [[ $suite == $script_name || ! -x $suite ]]; then
 		continue  # Skip this file and utils.zsh
@@ -18,6 +20,10 @@ for suite in *.zsh; do
 	{ ./$suite 2>&1 } \
 		> >(awk '/===|---|✔|✖︎|↪/{print}') \
 		> >(grep -F ↪ >> "$summaries")
+
+	if ! tail -n 1 "$summaries" | grep -Fq " 0 failed"; then
+		failing_suites+=$suite
+	fi
 done
 
 # Summarize the summaries
@@ -39,5 +45,9 @@ fi
 
 echo -e "\n${bright}~~~ Overall results ~~~${normal}"
 echo -e "${emoji} ${color}Ran $(( total_passed + total_failed )) tests: $total_passed passed, $total_failed failed${normal}"
+
+if [[ -n $failing_suites ]]; then
+	echo -e "   ${color}Failing suites: $failing_suites${normal}"
+fi
 
 rm $summaries
