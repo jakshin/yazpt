18c4d2ccbee45b19fc76944ef074a4f9c297ef1d
Jason Jackson <jasonjackson@pobox.com>
2020-04-21 14:34:09 -0600

    Tests for new Git repos with no commits

--- tests/git-new-repo.zsh
+++ tests/git-new-repo.zsh
@@ -12,13 +12,13 @@ YAZPT_VCS_ORDER=(git)
 test_case "New repo with no branches/commits"
 git init
 test_init_done
-contains_branch "master"
+contains_context "master"
 contains_status "no-upstream"
 
 test_case "In the .git directory of a new repo with no branches/commits"
 cd $(git rev-parse --git-dir)
 test_init_done
-contains_dim_branch "master"
+contains_dim_context "master"
 contains_status "no-upstream"
 contains "IN-GIT-DIR"
 
