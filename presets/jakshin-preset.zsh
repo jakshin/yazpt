dc5d9769d08edf1345f6a44668ad347b2c091284
Jason Jackson <jasonjackson@pobox.com>
2020-04-21 14:50:24 -0600

    A preset based on my personal default settings

--- presets/jakshin-preset.zsh
+++ presets/jakshin-preset.zsh
@@ -1,8 +1,8 @@
 # A preset based on my personal default settings for the last decade or so.
 #
-# It's maybe a bit weird to put the git branch/status on the left,
+# It's maybe a bit weird to put the Git context and status on the left,
 # so it shifts the current directory to the right when it's visible,
-# but I like that it makes it more visually obvious when I'm in a git repo.
+# but I like that it makes it more visually obvious when I'm in a Git repo.
 #
 # Showing the current directory in bright yellow makes a very clear visual divider
 # between each command, since commands rarely use bright yellow in their output.
