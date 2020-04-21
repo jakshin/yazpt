0a44dbf93055212dc23e408846484f3be8684105
Jason Jackson <jasonjackson@pobox.com>
2020-04-21 14:29:14 -0600

    Tests for externals in a Subversion working copy

--- tests/svn-externals.zsh
+++ tests/svn-externals.zsh
@@ -12,10 +12,10 @@ YAZPT_VCS_ORDER=(svn)
 # Test
 test_case "The external's branch is independent from the working copy's"
 test_init_done
-contains_branch "trunk"
+contains_context "trunk"
 cd yazpt-svn-ext
 test_init_done
-contains_branch "v1"
+contains_context "v1"
 
 test_case "The external's status is independent from the working copy's"
 svn lock lock-me.txt --force
