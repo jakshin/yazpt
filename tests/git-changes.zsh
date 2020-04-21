8dd47d14ca735fcbf07229ebcb508c1e0010556b
Jason Jackson <jasonjackson@pobox.com>
2020-04-21 14:35:38 -0600

    Tests for various types of file/directory changes in a Git repo

--- tests/git-changes.zsh
+++ tests/git-changes.zsh
@@ -15,77 +15,77 @@ function run_tests() {
 	mkdir ignored
 	cd ignored
 	test_init_done
-	contains_dim_branch "branch1"
+	contains_dim_context "branch1"
 	contains_status "clean"
 	contains "IGNORED"
 
 	test_case "With an empty untracked directory"
 	mkdir empty
 	test_init_done
-	contains_branch "branch1"
+	contains_context "branch1"
 	contains_status "clean"
 
 	test_case "With an untracked file"
 	echo untracked > untracked.txt
 	test_init_done
-	contains_branch "branch1"
+	contains_context "branch1"
 	contains_status "dirty"
 	rm -f untracked.txt  # Cleanup
 
 	test_case "With a modified/renamed/deleted ignored file"
 	test_init_done
-	contains_branch "branch1"
+	contains_context "branch1"
 	contains_status "clean"
 	echo ignored > ignored.txt
 	test_init_done
-	contains_branch "branch1"
+	contains_context "branch1"
 	contains_status "clean"
 	mv -v ignored.txt ignored.text
 	test_init_done
-	contains_branch "branch1"
+	contains_context "branch1"
 	contains_status "clean"
 	rm -fv ignored.text
 	test_init_done
-	contains_branch "branch1"
+	contains_context "branch1"
 	contains_status "clean"
 
 	test_case "With a deleted file (rm)"
 	rm -fv bar.txt
 	test_init_done
-	contains_branch "branch1"
+	contains_context "branch1"
 	contains_status "dirty"
 	git checkout .  # Cleanup
 
 	test_case "With a deleted file (git rm)"
 	git rm bar.txt
 	test_init_done
-	contains_branch "branch1"
+	contains_context "branch1"
 	contains_status "dirty"
 	git reset && git checkout bar.txt  # Cleanup
 
 	test_case "With a renamed file (git mv)"
 	git mv bar.txt new-name.txt
 	test_init_done
-	contains_branch "branch1"
+	contains_context "branch1"
 	contains_status "dirty"
 	git reset HEAD bar.txt new-name.txt && mv new-name.txt bar.txt  # Cleanup
 
 	test_case "With a modified file"
 	echo barrrr >> bar.txt
 	test_init_done
-	contains_branch "branch1"
+	contains_context "branch1"
 	contains_status "dirty"
 
 	test_case "With staged changes"
 	git add .
 	test_init_done
-	contains_branch "branch1"
+	contains_context "branch1"
 	contains_status "dirty"
 
 	test_case "With a commit that hasn't been pushed"
 	git commit -m "modified bar.txt"
 	test_init_done
-	contains_branch "branch1"
+	contains_context "branch1"
 	contains_status "diverged"
 	git reset --hard HEAD~1  # Cleanup
 
@@ -94,7 +94,7 @@ function run_tests() {
 	git reset HEAD~1
 	rm baz.txt
 	test_init_done
-	contains_branch "branch2"
+	contains_context "branch2"
 	contains_status "diverged"
 	git pull && git checkout branch1  # Cleanup
 }
