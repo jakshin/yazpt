0299185d7ae2bd09a9a14458211bd267bba93b5b
Jason Jackson <jasonjackson@pobox.com>
2020-04-30 14:03:57 -0600

    Tests for configurable prompt layout
    Now including $YAZPT_RLAYOUT -> $RPS1.

--- tests/layout.zsh
+++ tests/layout.zsh
@@ -66,5 +66,18 @@ test_case "Empty YAZPT_LAYOUT"
 YAZPT_LAYOUT=""
 test_init_done  # Standard tests suffice in this case
 
+test_case "YAZPT_RLAYOUT contains <exectime>"
+YAZPT_EXECTIME_CHAR=""
+YAZPT_EXECTIME_COLOR=21
+YAZPT_EXECTIME_MIN_SECONDS=1
+test_init_done "sleep 1"
+RPS1=${RPS1//2s/1s}  # Defend against flakiness
+equals RPS1 "$RPS1" "%{%F{21}%}1s%{%f%}"
+
+test_case "YAZPT_RLAYOUT is empty"
+RPS1="dummy"
+test_init_done "no-standard-tests"
+equals RPS1 "$RPS1" ""
+
 # Clean up
 after_tests
