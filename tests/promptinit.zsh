31e55b788fc37e310225f196b2e52399c3bdf61c
Jason Jackson <jasonjackson@pobox.com>
2020-04-30 14:04:22 -0600

    Tests for yazpt's integration with zsh's prompt theme system

--- tests/promptinit.zsh
+++ tests/promptinit.zsh
@@ -19,21 +19,25 @@ function run_test_variant_1() {
 	test_init_done "no-standard-tests"
 	is $default_yazpt_ps1
 	has_one_precmd_function
+	has_one_preexec_function
 
 	yazpt_load_preset jakshin
 	test_init_done "no-standard-tests"
 	is $jakshin_yazpt_ps1
 	has_one_precmd_function
+	has_one_preexec_function
 
 	prompt -h yazpt > /dev/null
 	test_init_done "no-standard-tests"
 	is $jakshin_yazpt_ps1
 	has_one_precmd_function
+	has_one_preexec_function
 
 	prompt -p "yazpt blues" > /dev/null
 	test_init_done "no-standard-tests"
 	is $jakshin_yazpt_ps1
 	has_one_precmd_function
+	has_one_preexec_function
 }
 
 function run_test_variant_2() {
@@ -41,37 +45,44 @@ function run_test_variant_2() {
 	test_init_done "no-standard-tests"
 	is $default_yazpt_ps1
 	has_one_precmd_function
+	has_one_preexec_function
 
 	yazpt_load_preset jakshin
 	test_init_done "no-standard-tests"
 	is $jakshin_yazpt_ps1
 	has_one_precmd_function
+	has_one_preexec_function
 
 	prompt -p "yazpt blues" > /dev/null
 	test_init_done "no-standard-tests"
 	is $jakshin_yazpt_ps1
 	has_one_precmd_function
+	has_one_preexec_function
 
 	prompt -h yazpt > /dev/null
 	test_init_done "no-standard-tests"
 	is $jakshin_yazpt_ps1
 	has_one_precmd_function
+	has_one_preexec_function
 }
 
 function run_test_variant_3() {
 	prompt -h yazpt > /dev/null
 	is $default_ps1
 	has_no_precmd_function
+	has_no_preexec_function
 
 	prompt yazpt jakshin
 	test_init_done "no-standard-tests"
 	is $jakshin_yazpt_ps1
 	has_one_precmd_function
+	has_one_preexec_function
 
 	prompt -p "yazpt blues" > /dev/null
 	test_init_done "no-standard-tests"
 	is $jakshin_yazpt_ps1
 	has_one_precmd_function
+	has_one_preexec_function
 }
 
 function run_test_variant_4() {
@@ -79,51 +90,57 @@ function run_test_variant_4() {
 	prompt -p "yazpt all" > /dev/null
 	is $default_ps1
 	has_no_precmd_function
+	has_no_preexec_function
 
 	prompt yazpt
 	test_init_done "no-standard-tests"
 	is $default_yazpt_ps1
 	has_one_precmd_function
+	has_one_preexec_function
 
 	prompt yazpt jakshin
 	test_init_done "no-standard-tests"
 	is $jakshin_yazpt_ps1
 	has_one_precmd_function
+	has_one_preexec_function
 }
 
 function run_test_variant_5() {
 	prompt -p "yazpt blues" > /dev/null
 	is $default_ps1
 	has_no_precmd_function
+	has_no_preexec_function
 
 	prompt yazpt
 	yazpt_load_preset jakshin
 	test_init_done "no-standard-tests"
 	is $jakshin_yazpt_ps1
 	has_one_precmd_function
+	has_one_preexec_function
 
 	prompt -h yazpt > /dev/null
 	test_init_done "no-standard-tests"
 	is $jakshin_yazpt_ps1
 	has_one_precmd_function
+	has_one_preexec_function
 }
 
 test_case "Integration with promptinit"
 for i in {1..5}; do
-	yazpt_plugin_unload
+	functions yazpt_plugin_unload > /dev/null && yazpt_plugin_unload
 	run_test_variant_$i
 done
 
 test_case "Integration with promptinit (yazpt loaded beforehand)"
 for i in {1..5}; do
-	yazpt_plugin_unload
+	functions yazpt_plugin_unload > /dev/null && yazpt_plugin_unload
 	source "$test_dir/../yazpt.zsh-theme"
 	run_test_variant_$i
 done
 
 test_case "Integration with promptinit (yazpt loaded beforehand, with preset)"
 for i in {1..5}; do
-	yazpt_plugin_unload
+	functions yazpt_plugin_unload > /dev/null && yazpt_plugin_unload
 	source "$test_dir/../yazpt.zsh-theme"
 	yazpt_load_preset spearmint
 	run_test_variant_$i
