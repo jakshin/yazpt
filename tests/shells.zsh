a9cc7c29b905bfc1544f348b0b1722ae1613e60c
Jason Jackson <jasonjackson@pobox.com>
2020-05-28 14:06:18 -0600

    Tests for trying to load yazpt with shells other than zsh

--- tests/shells.zsh
+++ tests/shells.zsh
@@ -14,8 +14,12 @@ function test_with_shell() {
 	local src_cmd=$2  # Optional
 
 	if which $shell > /dev/null; then
-		test_case "Loading with $shell"
+		test_case "Sourcing with $shell"
 		output="$($shell -c "${src_cmd:-source} $test_dir/../yazpt.zsh-theme" 2>&1)"
+		equals "Output" "$output" "Sorry, the yazpt prompt theme only works on zsh."
+
+		test_case "Executing with $shell"
+		output="$($shell "$test_dir/../yazpt.zsh-theme" 2>&1)"
 		equals "Output" "$output" "Sorry, the yazpt prompt theme only works on zsh."
 	fi
 }
@@ -35,7 +39,13 @@ if which rzsh > /dev/null; then
 	test_case "Loading with rzsh (zsh in restricted mode)"
 	output="$(rzsh -c "source $test_dir/../yazpt.zsh-theme" 2>&1)"
 	equals "Output" "$output" "Sorry, the yazpt prompt theme doesn't work on restricted zsh."
+	output="$(rzsh "$test_dir/../yazpt.zsh-theme" 2>&1)"
+	equals "Output" "$output" "Sorry, the yazpt prompt theme doesn't work on restricted zsh."
 fi
+
+test_case "Executing with zsh"
+output="$(zsh "$test_dir/../yazpt.zsh-theme" 2>&1)"
+equals "Output" "$output" "Please source this script instead of running it."
 
 # Clean up
 after_tests
