8de31b93236090dccfa8e9a81fd8815ee46d1e48
Jason Jackson <jasonjackson@pobox.com>
2020-06-09 23:10:33 -0600

    Tests for our handling of TFVC status characters

--- tests/tfvc-status-chars.zsh
+++ tests/tfvc-status-chars.zsh
@@ -26,9 +26,9 @@ test_init_done
 excludes_tfvc_status
 excludes "%F{$YAZPT_VCS_STATUS_CLEAN_COLOR}"
 
-test_case "Empty YAZPT_VCS_STATUS_DIRTY_CHAR (YAZPT_VCS_TFVC_CHECK_LOCKS=true)"
+test_case "Empty YAZPT_VCS_STATUS_DIRTY_CHAR (YAZPT_CHECK_TFVC_LOCKS=true)"
 reset_tfvc_status_chars
-YAZPT_VCS_TFVC_CHECK_LOCKS=true
+YAZPT_CHECK_TFVC_LOCKS=true
 function .yazpt_parse_pendingchanges_tf1() { _yazpt_tfvc_status=ny }
 function zstat() { stat=(24) }
 function .yazpt_parse_properties_tf1() {
@@ -43,9 +43,9 @@ test_init_done
 excludes_tfvc_status
 excludes "%F{$YAZPT_VCS_STATUS_DIRTY_COLOR}"
 
-test_case "Empty YAZPT_VCS_STATUS_LOCKED_CHAR (YAZPT_VCS_TFVC_CHECK_LOCKS=true)"
+test_case "Empty YAZPT_VCS_STATUS_LOCKED_CHAR (YAZPT_CHECK_TFVC_LOCKS=true)"
 reset_tfvc_status_chars
-YAZPT_VCS_TFVC_CHECK_LOCKS=true
+YAZPT_CHECK_TFVC_LOCKS=true
 function .yazpt_parse_pendingchanges_tf1() { _yazpt_tfvc_status=yn }
 test_init_done
 contains "⊠"
@@ -55,9 +55,9 @@ test_init_done
 excludes_tfvc_status
 excludes "%F{$YAZPT_VCS_STATUS_LOCKED_COLOR}"
 
-test_case "Empty YAZPT_VCS_STATUS_DIRTY_CHAR (YAZPT_VCS_TFVC_CHECK_LOCKS=false)"
+test_case "Empty YAZPT_VCS_STATUS_DIRTY_CHAR (YAZPT_CHECK_TFVC_LOCKS=false)"
 reset_tfvc_status_chars
-YAZPT_VCS_TFVC_CHECK_LOCKS=false
+YAZPT_CHECK_TFVC_LOCKS=false
 unfunction .yazpt_parse_pendingchanges_tf1  # Shouldn't get called
 test_init_done
 contains "⚑"
@@ -70,7 +70,7 @@ excludes "%F{$YAZPT_VCS_STATUS_LOCKED_COLOR}"
 
 test_case "Empty YAZPT_VCS_STATUS_UNKNOWN_CHAR (parse failure)"
 reset_tfvc_status_chars
-YAZPT_VCS_TFVC_CHECK_LOCKS=true
+YAZPT_CHECK_TFVC_LOCKS=true
 function .yazpt_parse_pendingchanges_tf1() { _yazpt_tfvc_status=nn }
 test_init_done
 contains "⌀"
