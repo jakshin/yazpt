af5aaae3f4e771a565534ec8c2fe3e06039e3df4
Jason Jackson <jasonjackson@pobox.com>
2020-06-09 23:10:12 -0600

    Tests for locking files in a TFVC workspace

--- tests/tfvc-locking.zsh
+++ tests/tfvc-locking.zsh
@@ -11,14 +11,14 @@ YAZPT_VCS_ORDER=(tfvc)
 # Test
 test_case "With a locked file"
 $tf_cli lock ${=tf_args} -lock:checkin lock-me.txt
-YAZPT_VCS_TFVC_CHECK_LOCKS=true
+YAZPT_CHECK_TFVC_LOCKS=true
 test_init_done
 contains_status "locked"
 cd folder
 test_init_done
 contains_status "locked"
 cd ..
-YAZPT_VCS_TFVC_CHECK_LOCKS=false
+YAZPT_CHECK_TFVC_LOCKS=false
 test_init_done
 contains_status "dirty"
 cd folder
