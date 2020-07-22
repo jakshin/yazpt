768f5265d198a9a2202a51cd7c211c4652fc22a7
Jason Jackson <jasonjackson@pobox.com>
2020-07-22 14:15:45 -0600

    Add a couple of stub scripts

---
+++ devtools/palette.zsh
@@ -0,0 +1,9 @@
+# Maps colors 16 through 255 in the 256-color palette to RGB values.
+# Based on https://jonasjacek.github.io/colors and https://stackoverflow.com/a/27165165.
+# FIXME this is only a stub so far
+
+declare -A indexed_colors=(
+  1 'one'
+  2 'two'
+  3 'three'
+)
