487b09c3b572c40bf575725574888be39f79a839
Jason Jackson <jasonjackson@pobox.com>
2020-07-25 17:13:44 -0600

    Handle Debian 10's weird GNOME Terminal better (probably Mintty too)

--- detect-bg.zsh
+++ detect-bg.zsh
@@ -35,22 +35,34 @@ source "$script_dir/yazpt.zsh-theme"
 		fg=$(( arr[1] + arr[2] + arr[3] ))
 		bg=$(( arr[4] + arr[5] + arr[6] ))
 	else
-		local delim=$'\a' fg_arr bg_arr i
-		[[ $OSTYPE == "cygwin" ]] && delim='\'  # FIXME maybe this is actually Mintty-specific?
+		# local delim=$'\a' fg_arr bg_arr i
+		# [[ $OSTYPE == "cygwin" ]] && delim='\'  # FIXME maybe this is actually Mintty-specific?
 
 		if [[ -t 0 ]]; then
 			local tty_settings="$(stty -g)"  # Save TTY settings
 			stty -echo                       # Turn echo to TTY off
 		fi
 
-		echo -en '\e]10;?\a'; IFS=:/ read -t 0.1 -d $delim -A fg_arr  # Get foreground color
-		echo -en '\e]11;?\a'; IFS=:/ read -t 0.1 -d $delim -A bg_arr  # Get background color
-		[[ -n $debug ]] && echo "fg_arr = ${fg_arr[2,4]//$'\e'/}, bg_arr = ${bg_arr[2,4]//$'\e'/}"
-		[[ -z $tty_settings ]] || stty "$tty_settings"  # Restore TTY settings
+		# echo -en '\e]10;?\a'; IFS=:/ read -t 0.1 -d $delim -A fg_arr  # Get foreground color
+		# echo -en '\e]11;?\a'; IFS=:/ read -t 0.1 -d $delim -A bg_arr  # Get background color
+		# [[ -n $debug ]] && echo "fg_arr = ${fg_arr[2,4]//$'\e'/}, bg_arr = ${bg_arr[2,4]//$'\e'/}"
+		# [[ -z $tty_settings ]] || stty "$tty_settings"  # Restore TTY settings
+		#
+		# for (( i=2; i <= 4; i++ )); do
+		# 	(( fg+=16#${fg_arr[$i]//[^a-zA-Z0-9]/} ))
+		# 	(( bg+=16#${bg_arr[$i]//[^a-zA-Z0-9]/} ))
+		# done
 
-		for (( i=2; i <= 4; i++ )); do
-			(( fg+=16#${fg_arr[$i]//[^a-zA-Z0-9]/} ))
-			(( bg+=16#${bg_arr[$i]//[^a-zA-Z0-9]/} ))
+		local fg_arr bg_arr i
+		echo -en '\e]10;?\a'; .yazpt_read_term_color fg_arr  # Get foreground color
+		echo -en '\e]11;?\a'; .yazpt_read_term_color bg_arr  # Get background color
+		[[ -z $tty_settings ]] || stty "$tty_settings"       # Restore TTY settings
+
+		[[ -n $debug ]] && echo "fg_arr = $fg_arr, bg_arr = $bg_arr"
+
+		for (( i=1; i <= 3; i++ )); do
+			(( fg+=16#${fg_arr[$i]} ))
+			(( bg+=16#${bg_arr[$i]} ))
 		done
 	fi
 
@@ -66,6 +78,29 @@ source "$script_dir/yazpt.zsh-theme"
 		echo "fg: $fg, bg: $bg"
 		echo "background: $yazpt_bg"
 	fi
+}
+
+# \e]10;rgb:c10a/c10b/c10a\a
+# \e]10;rgb:eeee/eeee/ecec\e\
+function .yazpt_read_term_color() {
+	local var=$1  # The array variable to store the terminal's response in
+
+	local ch colon=false str
+	while read -t 0.1 -k ch; do
+		if [[ $ch == $'\a' || $ch == '\' ]]; then
+			break
+		elif [[ $colon == true ]]; then
+			if [[ $ch == $'\e' ]]; then
+				break
+			else
+				str+="$ch"
+			fi
+		elif [[ $ch == ':' ]]; then
+			colon=true
+		fi
+	done
+
+	eval "$var=(\${(s:/:)str})"
 }
 
 .yazpt_detect_bg true
